f4dd3ea6a250503be1addb9cee0a2d1138da7c6b
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-08-21 21:07:57 -0300

    criando middleware pra verificar se o user existe

--- index.js
+++ index.js
@@ -11,31 +11,53 @@ server.use((req, res, next) => {
   return next();
 });
 
+checkUserExists = (req, res, next) => {
+  if (!req.body.name) {
+    return res.status(400).json({ error: "username is required" });
+  }
+
+  return next();
+};
+
+checkUserExistsInArray = (req, res, next) => {
+  const user = users[req.params.index];
+  if (!user) {
+    return res.status(400).json({ error: "user doesn't exist" });
+  }
+
+  req.user = user;
+
+  return next();
+};
+
 server.get("/users", (req, res) => {
   return res.json(users);
 });
 
-server.get("/users/:index", (req, res) => {
-  const { index } = req.params;
-
-  return res.json(users[index]);
+server.get("/users/:index", checkUserExistsInArray, (req, res) => {
+  return res.json(req.user);
 });
 
-server.post("/users", (req, res) => {
+server.post("/users", checkUserExists, (req, res) => {
   const { name } = req.body;
   users.push(name);
   return res.json(users);
 });
 
-server.put("/users/:index", (req, res) => {
-  const { name } = req.body;
-  const { index } = req.params;
+server.put(
+  "/users/:index",
+  checkUserExists,
+  checkUserExistsInArray,
+  (req, res) => {
+    const { name } = req.body;
+    const { index } = req.params;
 
-  users[index] = name;
-  return res.json(users);
-});
+    users[index] = name;
+    return res.json(users);
+  }
+);
 
-server.delete("/users/:index", (req, res) => {
+server.delete("/users/:index", checkUserExistsInArray, (req, res) => {
   const { index } = req.params;
 
   users.splice(index, 1);
